858c39fc48a85549e51e85a907c12a98f550d937
00komiya <114364060+00komiya@users.noreply.github.com>
2023-01-23 13:31:34 +0000

    [update]books_controller/edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,14 +35,14 @@ class BooksController < ApplicationController
     end
   end
 
-  def edit
-    @book = Book.find(params[:id])
-   if @book.user == current_user
-      render "edit"
-   else
-     redirect_to books_path
-   end
-  end
+  # def edit
+  #   @book = Book.find(params[:id])
+  # if @book.user == current_user
+  #     render "edit"
+  # else
+  #   redirect_to books_path
+  # end
+  # end
 
   def update
     @book = Book.find(params[:id])
